fc4ddfb591c72abac9224eea40f89c18e3b5bc62
saanvi.code <sa.manni.g9@gmail.com>
2025-04-27 11:20:26 +0530

    k

---
+++ set2.py
@@ -0,0 +1,8 @@
+set1={"purple","purple","blue","green"}
+set2={"yellow","gold","purple","green","blue"}
+print("the original sets are :",end="")
+print(set1,set2)
+print(set2.intersection(set1))
+print(set2.union(set1))
+print(set2.difference(set1))
+print(set1.symmetric_difference(set2))
